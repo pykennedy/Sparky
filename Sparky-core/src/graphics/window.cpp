5cf67951a7ded40bed845c5790ec85975cc42c10
Peter <PYKennedy@gmail.com>
2017-02-05 16:26:23 -0800

    formatting fixes

--- Sparky-core/src/graphics/window.cpp
+++ Sparky-core/src/graphics/window.cpp
@@ -1,56 +1,56 @@
 #include "window.h"
 
 namespace sparky {
-	namespace graphics {
-		
-		void windowResize(GLFWwindow *window, int width, int height);
+namespace graphics {
 
-		Window::Window(const char *title, int width, int height) {
-			m_Title = title;
-			m_Width = width;
-			m_Height = height;
-			if (!init())
-				glfwTerminate();
-		}
+void windowResize(GLFWwindow *window, int width, int height);
 
-		Window::~Window() {
-			glfwTerminate();
-		}
+Window::Window(const char *title, int width, int height) {
+	m_Title = title;
+	m_Width = width;
+	m_Height = height;
+	if (!init())
+		glfwTerminate();
+}
 
-		bool Window::init() {
-			if (!glfwInit()) {
-				std::cout << "Failed to initialize GLFW!" << std::endl;
-				return false;
-			}
+Window::~Window() {
+	glfwTerminate();
+}
 
-			m_Window = glfwCreateWindow(m_Width, m_Height, m_Title, NULL, NULL);
-			if (!m_Window) {
-				glfwTerminate();
-				std::cout << "Failed to create GLFW window!" << std::endl;
-				return false;
-			}
-			glfwMakeContextCurrent(m_Window);
-			glfwSetWindowSizeCallback(m_Window, windowResize);
-
-			return true;
-		}
-
-		void Window::clear() const {
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		}
-
-		void Window::update() {
-			glfwPollEvents();
-//			glfwGetFramebufferSize(m_Window, &m_Width, &m_Height);
-			glfwSwapBuffers(m_Window);
-		}
-
-		bool Window::closed() const {
-			return glfwWindowShouldClose(m_Window) == 1;
-		}
-
-		void windowResize(GLFWwindow *window, int width, int height) {
-			glViewport(0, 0, width, height);
-		}
+bool Window::init() {
+	if (!glfwInit()) {
+		std::cout << "Failed to initialize GLFW!" << std::endl;
+		return false;
 	}
+
+	m_Window = glfwCreateWindow(m_Width, m_Height, m_Title, NULL, NULL);
+	if (!m_Window) {
+		glfwTerminate();
+		std::cout << "Failed to create GLFW window!" << std::endl;
+		return false;
+	}
+	glfwMakeContextCurrent(m_Window);
+	glfwSetWindowSizeCallback(m_Window, windowResize);
+
+	return true;
+}
+
+void Window::clear() const {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void Window::update() {
+	glfwPollEvents();
+	//glfwGetFramebufferSize(m_Window, &m_Width, &m_Height);
+	glfwSwapBuffers(m_Window);
+}
+
+bool Window::closed() const {
+	return glfwWindowShouldClose(m_Window) == 1;
+}
+
+void windowResize(GLFWwindow *window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+}
 }
--- Sparky-core/src/graphics/window.h
+++ Sparky-core/src/graphics/window.h
@@ -4,30 +4,30 @@
 #include<iostream>
 
 namespace sparky {
-	namespace graphics{
-		
-		class Window {
-		private:
-			const char *m_Title;
-			int m_Width, m_Height;
-			GLFWwindow *m_Window;
-			bool m_Closed;
-		public:
-			Window(const char *title, int width, int height);
-			~Window();
-			bool closed() const;
-			void update();
-			void clear() const;
+namespace graphics {
 
-			inline int getWidth() const {
-				return m_Width;
-			}
-			inline int getHeight() const {
-				return m_Height;
-			}
-		private:
-			bool init();
-		};
+class Window {
+private:
+	const char *m_Title;
+	int m_Width, m_Height;
+	GLFWwindow *m_Window;
+	bool m_Closed;
+public:
+	Window(const char *title, int width, int height);
+	~Window();
+	bool closed() const;
+	void update();
+	void clear() const;
 
+	inline int getWidth() const {
+		return m_Width;
 	}
- }
+	inline int getHeight() const {
+		return m_Height;
+	}
+private:
+	bool init();
+};
+
+}
+}
