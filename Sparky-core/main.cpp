3cc78fa05a4e1b5f745e35f63324b4d6db3f43d7
Peter <PYKennedy@gmail.com>
2017-02-11 19:16:19 -0800

    ep 2&3

--- Sparky-core/main.cpp
+++ Sparky-core/main.cpp
@@ -1,5 +1,5 @@
-#include <GLFW/glfw3.h>
 #include <iostream>
+//#include <GLFW/glfw3.h>
 
 #include "src/graphics/window.h"
 
@@ -11,15 +11,32 @@ int main() {
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+
 	while (!window.closed()) {
-		std::cout << window.getWidth() << ", " << window.getHeight() << std::endl;
 		window.clear();
+		double x, y;
+		window.getMousePosition(x, y);
+		std::cout << x << ", " << y << std::endl;
+		if (window.isKeyPressed(GLFW_KEY_A)) {
+			std::cout << "Key Pressed!" << std::endl;
+		}
+		if (window.isMouseButtonPressed(GLFW_MOUSE_BUTTON_RIGHT)) {
+			std::cout << "Mouse Pressed!" << std::endl;
+		}
+#if 1
 		glBegin(GL_QUADS);
 		glVertex2f(-0.5f, -0.5f);
 		glVertex2f(-0.5f, 0.5f);
 		glVertex2f(0.5f, 0.5f);
 		glVertex2f(0.5f, -0.5f);
 		glEnd();
+#else
+		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
+#endif
 		window.update();
 	}
 	//system("PAUSE");
--- Sparky-core/src/graphics/window.cpp
+++ Sparky-core/src/graphics/window.cpp
@@ -3,7 +3,7 @@
 namespace sparky {
 namespace graphics {
 
-void windowResize(GLFWwindow *window, int width, int height);
+void window_resize(GLFWwindow *window, int width, int height);
 
 Window::Window(const char *title, int width, int height) {
 	m_Title = title;
@@ -11,6 +11,14 @@ Window::Window(const char *title, int width, int height) {
 	m_Height = height;
 	if (!init())
 		glfwTerminate();
+
+	for (int i = 0; i < MAX_KEYS; i++) {
+		m_Keys[i] = false;
+	}
+
+	for (int i = 0; i < MAX_BUTTONS; i++) {
+		m_MouseButtons[i] = false;
+	}
 }
 
 Window::~Window() {
@@ -30,9 +38,41 @@ bool Window::init() {
 		return false;
 	}
 	glfwMakeContextCurrent(m_Window);
-	glfwSetWindowSizeCallback(m_Window, windowResize);
+	glfwSetWindowUserPointer(m_Window, this);
+	glfwSetWindowSizeCallback(m_Window, window_resize);
+	glfwSetKeyCallback(m_Window, key_callback);
+	glfwSetMouseButtonCallback(m_Window, mouse_button_callback);
+	glfwSetCursorPosCallback(m_Window, cursor_position_callback);
+
+	if (glewInit() != GLEW_OK) {
+		std::cout << "Could not initialize GLEW!" << std::endl;
+		return false;
+	}
+
+	std::cout << "OpenGL " << glGetString(GL_VERSION) << std::endl;
 
 	return true;
+}
+
+bool Window::isKeyPressed(unsigned int keycode) const {
+	//TODO: Log this!
+	if (keycode >= MAX_KEYS) {
+		return false;
+	}
+	return m_Keys[keycode];
+}
+
+bool Window::isMouseButtonPressed(unsigned int button) const {
+	//TODO: Log this!
+	if (button >= MAX_BUTTONS) {
+		return false;
+	}
+	return m_MouseButtons[button];
+}
+
+void Window::getMousePosition(double &x, double &y) const {
+	x = mx;
+	y = my;
 }
 
 void Window::clear() const {
@@ -49,8 +89,26 @@ bool Window::closed() const {
 	return glfwWindowShouldClose(m_Window) == 1;
 }
 
-void windowResize(GLFWwindow *window, int width, int height) {
+void window_resize(GLFWwindow *window, int width, int height) {
 	glViewport(0, 0, width, height);
+}
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action
+									, int mods) {
+	Window *win = (Window*) glfwGetWindowUserPointer(window);
+	win->m_Keys[key] = action != GLFW_RELEASE;
+}
+
+void mouse_button_callback(GLFWwindow *window, int button, int action
+													 , int mods) {
+	Window *win = (Window*)glfwGetWindowUserPointer(window);
+	win->m_MouseButtons[button] = action != GLFW_RELEASE;
+}
+
+void cursor_position_callback(GLFWwindow *window, double xpos, double ypos) {
+	Window *win = (Window*)glfwGetWindowUserPointer(window);
+	win->mx = xpos;
+	win->my = ypos;
 }
 }
 }
