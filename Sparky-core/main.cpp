2a3529026326af6145cb6dd8bc2fbfd4cca63af4
Peter <PYKennedy@gmail.com>
2017-02-26 16:32:05 -0800

    ep 5 & 5.5

--- Sparky-core/main.cpp
+++ Sparky-core/main.cpp
@@ -15,14 +15,24 @@ int main() {
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 
-	vec3 a(1.0f, 2.0f, 5.0f);
-	vec3 b(2, 4, 6);
+	vec4 a(0.2f, 0.3f, 0.8f, 1.0f);
+	vec4 b(0.2f, 0.3f, 0.8f, 1.0f);
 
-	vec3 c = a + b;
+	vec4 c = a + b;
+
+	mat4 position = mat4::translation(vec3(2, 3, 4));
+	position *= mat4::identity();
+
+	position.elements[12] = 2.0f;
+
+	vec4 c0 = position.columns[3];
+	vec4 c1 = position.getColumn(3);
+	std::cout << c0 << std::endl;
+	std::cout << c1 << std::endl;
 
 	while (!window.closed()) {
 		window.clear();
-		std::cout << a << std::endl;
+		//std::cout << a << std::endl;
 #if 1
 		glBegin(GL_QUADS);
 		glVertex2f(-0.5f, -0.5f);
--- Sparky-core/src/maths/vec4.cpp
+++ Sparky-core/src/maths/vec4.cpp
@@ -3,17 +3,11 @@
 namespace sparky {
 namespace maths {
 
-vec4::vec4() {
-	x = 0.0f;
-	y = 0.0f;
-	z = 0.0f;
-	w = 0.0f;
-}
-
 vec4::vec4(const float &x, const float &y, const float &z, const float &w) {
 	this->x = x;
 	this->y = y;
 	this->z = z;
+	this->w = w;
 }
 
 vec4& vec4::add(const vec4 &other) {
