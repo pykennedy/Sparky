2a324d1cecec167966538b5373ad87573d561be7
Peter <PYKennedy@gmail.com>
2017-03-12 13:00:57 -0700

    ep10

--- Sparky-core/src/maths/mat4.cpp
+++ Sparky-core/src/maths/mat4.cpp
@@ -1,5 +1,4 @@
 #include "mat4.h"
-#include "maths.h"
 
 namespace sparky {
 namespace maths {
@@ -23,47 +22,52 @@ mat4 mat4::identity() {
 	return mat4(1.0f);
 }
 
-mat4& mat4::multiply(const mat4 &other) {
+mat4& mat4::multiply(const mat4& other) {
+	float data[16];
 	for (int y = 0; y < 4; y++) {
 		for (int x = 0; x < 4; x++) {
 			float sum = 0.0f;
 			for (int e = 0; e < 4; e++) {
 				sum += elements[x + e * 4] * other.elements[e + y * 4];
 			}
-			elements[x + y * 4] = sum;
+			data[x + y * 4] = sum;
 		}
 	}
+	memcpy(elements, data, 4 * 4 * sizeof(float));
+
 	return *this;
 }
 
-mat4 operator*(mat4 left, const mat4 &right) {
-	mat4 result = left.multiply(right);
-	return result;
+mat4 operator*(mat4 left, const mat4& right) {
+	return left.multiply(right);
 }
 
-mat4& mat4::operator*=(const mat4 &other) {
+mat4& mat4::operator*=(const mat4& other) {
 	return multiply(other);
 }
 
-mat4 mat4::orthographic(float left, float right, float bottom, float top
-									, float near, float far) {
+mat4 mat4::orthographic(float left, float right, float bottom, float top, float near, float far) {
 	mat4 result(1.0f);
 
 	result.elements[0 + 0 * 4] = 2.0f / (right - left);
+
 	result.elements[1 + 1 * 4] = 2.0f / (top - bottom);
+
 	result.elements[2 + 2 * 4] = 2.0f / (near - far);
-	
+
 	result.elements[0 + 3 * 4] = (left + right) / (left - right);
 	result.elements[1 + 3 * 4] = (bottom + top) / (bottom - top);
 	result.elements[2 + 3 * 4] = (far + near) / (far - near);
 
 	return result;
 }
+
 mat4 mat4::perspective(float fov, float aspectRatio, float near, float far) {
 	mat4 result(1.0f);
 
 	float q = 1.0f / tan(toRadians(0.5f * fov));
 	float a = q / aspectRatio;
+
 	float b = (near + far) / (near - far);
 	float c = (2.0f * near * far) / (near - far);
 
@@ -76,7 +80,7 @@ mat4 mat4::perspective(float fov, float aspectRatio, float near, float far) {
 	return result;
 }
 
-mat4 mat4::translation(const vec3 &translation) {
+mat4 mat4::translation(const vec3& translation) {
 	mat4 result(1.0f);
 
 	result.elements[0 + 3 * 4] = translation.x;
@@ -86,18 +90,9 @@ mat4 mat4::translation(const vec3 &translation) {
 	return result;
 }
 
-mat4 mat4::scale(const vec3 &scale) {
+mat4 mat4::rotation(float angle, const vec3& axis) {
 	mat4 result(1.0f);
 
-	result.elements[0 + 3 * 4] = scale.x;
-	result.elements[1 + 1 * 4] = scale.y;
-	result.elements[2 + 2 * 4] = scale.z;
-
-	return result;
-}
-
-mat4 mat4::rotation(float angle, const vec3 &axis) {
-	mat4 result(1.0f);
 	float r = toRadians(angle);
 	float c = cos(r);
 	float s = sin(r);
@@ -118,6 +113,16 @@ mat4 mat4::rotation(float angle, const vec3 &axis) {
 	result.elements[0 + 2 * 4] = x * z * omc + y * s;
 	result.elements[1 + 2 * 4] = y * z * omc - x * s;
 	result.elements[2 + 2 * 4] = z * omc + c;
+
+	return result;
+}
+
+mat4 mat4::scale(const vec3& scale) {
+	mat4 result(1.0f);
+
+	result.elements[0 + 0 * 4] = scale.x;
+	result.elements[1 + 1 * 4] = scale.y;
+	result.elements[2 + 2 * 4] = scale.z;
 
 	return result;
 }
